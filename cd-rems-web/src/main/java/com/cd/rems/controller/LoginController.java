713f3a0dafa0269e861ffd4ab22626b18229e2e0
errorxxx <1010852016@qq.com>
2019-11-09 22:07:16 +0800

    前端页面访问

--- cd-rems-web/src/main/java/com/cd/rems/controller/LoginController.java
+++ cd-rems-web/src/main/java/com/cd/rems/controller/LoginController.java
@@ -2,11 +2,13 @@ package com.cd.rems.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-    @RequestMapping("/toLogin")
+    @RequestMapping(value = "/toLogin", method = RequestMethod.GET)
     public String test() {
         return "index";
     }
